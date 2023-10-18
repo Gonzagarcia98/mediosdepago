131dcc179b9552977b216081d5e24934bd7fa47a
Gonzagarcia98 <138820538+Gonzagarcia98@users.noreply.github.com>
2023-10-18 12:27:55 -0300

    Add files via upload

--- mediosdepago.py
+++ mediosdepago.py
@@ -1,18 +1,24 @@
 import streamlit as st
 
+st.title("Tarifas de medios de pago")
 
+st.write("---")
 st.image("imagenes/MERCADOPAGO.png")
 with st.expander("Mercado Pago", False):
     st.write("Info sobre costos de Mercado Pago")
+st.write("---")
 
 st.image("imagenes/PAYWAY.png")
 with st.expander("PayWay", False):    
     st.write("Info sobre costos de PayWay")
+st.write("---")
 
 st.image("imagenes/MODO.png")
 with st.expander("Modo", False):
     st.write("Info sobre costos de Modo")
+st.write("---")
 
-st.image("imagenes/UALA (600 x 300 px).png")
+st.image("imagenes/UALA.png")
 with st.expander("Uala", False):
     st.write("Info sobre costos de Uala")
+st.write("---")
