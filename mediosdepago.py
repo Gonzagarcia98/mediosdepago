39462422859feccbd4f79c99f851853a97272433
Gonzagarcia98 <138820538+Gonzagarcia98@users.noreply.github.com>
2023-10-18 13:13:28 -0300

    Add files via upload

--- mediosdepago.py
+++ mediosdepago.py
@@ -1,24 +1,7 @@
 import streamlit as st
 
-# Add the background CSS using st.markdown
-st.markdown(
-    """
-    <style>
-        body {
-            background-image: url("https://github.com/Gonzagarcia98/mediosdepago/blob/main/imagenes/TARIFAS%20UALA%201.png");
-            background-size: cover;
-            background-repeat: no-repeat;
-            background-attachment: fixed;
-        }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
-
-# The rest of your Streamlit app content
-st.title("Streamlit App with Background Image")
-st.write("This is the main content of your app.")
-
+# Agrega la imagen de fondo
+st.image('https://github.com/Gonzagarcia98/mediosdepago/blob/main/imagenes/TARIFAS%20UALA%201.png', use_column_width=True)
 
 st.markdown("<h1 style='text-align: center; font-size: 54px; font-family: Verdana, sans-serif;'>Tarifas de medios de pago</h1>", unsafe_allow_html=True)
 
