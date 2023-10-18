5edea9cdf409d41fe112d862b462f0c4815f7cb0
Gonzagarcia98 <138820538+Gonzagarcia98@users.noreply.github.com>
2023-10-18 11:33:41 -0300

    Add files via upload

--- mediosdepago.py
+++ mediosdepago.py
@@ -5,13 +5,13 @@ col1, col2= st.columns([2,2])
 with col2:
 
     with st.expander("Mercado Pago", True):
-        st.write()
+        st.write("Info sobre costos de Mercado Pago")
     with st.expander("PayWay", True):
-        st.write()
+        st.write("Info sobre costos de PayWay")
     with st.expander("Modo", True):
-        st.write()
+        st.write("Info sobre costos de Modo")
     with st.expander("Uala", True):
-        st.write()
+        st.write("Info sobre costos de Uala")
 
 with col1:
 
