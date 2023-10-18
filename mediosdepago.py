663fa3935547bdaee43282eb5cb9c6936dd1834d
Gonzagarcia98 <138820538+Gonzagarcia98@users.noreply.github.com>
2023-10-18 11:57:57 -0300

    Add files via upload

--- mediosdepago.py
+++ mediosdepago.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 
 
-with st.expander("**Mercado Pago**", False, font="Sans serif 36px"):
+with st.expander("Mercado Pago", False):
     st.write("Info sobre costos de Mercado Pago")
 with st.expander("**PayWay**", False):    
     st.write("Info sobre costos de PayWay")
@@ -10,3 +10,20 @@ with st.expander("**Modo**", False):
     st.write("Info sobre costos de Modo")
 with st.expander("**Uala**", False):
     st.write("Info sobre costos de Uala")
+
+st.write(
+    """
+    <style>
+        .expander-label {
+            font-size: 24px; /* Cambia el tamaño de la fuente según tus preferencias */
+        }
+        .expander-content {
+            font-size: 18px; /* Cambia el tamaño de la fuente del contenido según tus preferencias */
+        }
+    </style>
+    """
+)
+
+# Crear el expander
+with st.expander("Expander con fuente personalizada", expanded=True):
+    st.write("Este es el contenido del expander.")
